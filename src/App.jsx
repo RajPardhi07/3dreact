9809d8c94488f580020c50bf2b4b6e2bd9e704c5
RajPardhi07 <132877302+RajPardhi07@users.noreply.github.com>
2024-11-07 22:27:47 +0530

    chnages in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
   // }
 
   return (
-    <div id="main" className="scroll-container fixed top-0 left-0 overflow-scroll w-full h-[100vh]" >
+    <div id="main" className="scroll-container fixed top-0 left-0 overflow-scroll w-full h-[1105px]" >
 
       {/* {
         !show3dmodel && (
